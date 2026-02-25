0fd264985f8653836876c0b97db55a092a46a802
TestLicense2610 <testlicense9191@gmail.com>
2026-02-25 21:36:13 +0700

    css

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
+  output: 'export',
+  
+  // Thêm dòng này để khớp với tên Repository trên GitHub của bạn
+  basePath: '/React_GitHubAction', 
   
   typescript: {
     ignoreBuildErrors: true,
   },
-  
   images: {
     unoptimized: true,
   },
